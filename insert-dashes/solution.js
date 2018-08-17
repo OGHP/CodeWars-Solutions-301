7616dd210afb3daf3840d8a34bda8352d7e3c9a8
Heather Palmergit config --global user.email heatherpa@zillowgroup.com <palmerheather78@gmail.com>
2018-08-16 18:59:46 -0700

    Final solution for insert-dashes challenge

--- insert-dashes/solution.js
+++ insert-dashes/solution.js
@@ -2,16 +2,15 @@
 
 function insertDash(num) {
     let newString = '';
-    for (let i = 0; i < num.length; i++) {
-        //% returns the remainder left over when divided by 2
-        //checking to see if the first number is ODD
-        newString = newString + num[i];
-        //% returns the remainder left over when divided by 2
-            //checking to see if the first number is ODD
-        if (num[i] % 2 !== 0) {
-            if (num[i + 1] % 2 !== 0) {
-                newString = newString + '-';
-            };
+    let dashedString = num.toString();
+    for (let i = 0; i < dashedString.length; i++) {
+
+        newString = newString + dashedString[i];
+
+        if (dashedString[i] % 2 !== 0 && dashedString[i + 1] % 2 !== 0 && i + 1 < dashedString.length) {
+            newString = newString + '-';
         };
-    };
+    }
     return newString;
+};
+

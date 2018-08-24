2088a786af3b7c611a81954dfc5e3664d4b421d9
Heather <heatherpa@zillowgroup.com>
2018-08-24 14:15:59 -0700

    Added longer solution for study purposes

--- reverse-string/solution.js
+++ reverse-string/solution.js
@@ -1,3 +1,12 @@
 function solution(str) {
     return str.split("").reverse().join("");
 }
+
+//long solution
+// function solution(str) {
+//     let reverseString = '';  
+//     for (let i = str.length-1; i >=0; i--){
+//     reverseString += str[i];
+//     }
+//   return reverseString;
+// }
